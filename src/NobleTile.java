c02b38e86f4db8c953dea803f5e4e36474fcb0e1
josh <joshkuan.yiwei@users.noreply.github.com>
2026-02-23 16:49:43 +0800

    added constructor and getPoints

--- src/NobleTile.java
+++ src/NobleTile.java
@@ -6,21 +6,24 @@ public class NobleTile {
     private HashMap<Gem, Integer> tokens = new HashMap<>(6);
     private static final int POINTS = 3;
 
-    public NobleTile() {
-        for (Gem g : Gem.values()) {
-            this.tokens.put(g, 0);
-        }
+    public NobleTile(Gem gem, int Diamond, int Ruby, int Sapphire, int Emerald, int Onyx) {
+        tokens.put(Gem.Diamond, Diamond);
+        tokens.put(Gem.Ruby, Ruby);
+        tokens.put(Gem.Sapphire, Sapphire);
+        tokens.put(Gem.Emerald, Emerald);
+        tokens.put(Gem.Onyx, Onyx);
+        tokens.put(Gem.Gold, 0);
     }
-
-    // public NobleTile() {
-
-    // }
-
+    
     public HashMap<Gem, Integer> getTokens() {
         return this.tokens;
     }
 
     public void setTokens(Gem g, int amount) {
         this.tokens.put(g, amount);
+    }
+
+    public int getToken() {
+        return this.POINTS;
     }
 }
