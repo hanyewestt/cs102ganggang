7dc7418c7ce7cf6e91896751be13806dc9c84326
pestmant-uni <ruyhan.chua.2025@computing.smu.edu.sg>
2026-03-09 17:28:57 +0800

    Added printBoard() & changed Game variables to static

--- src/app/Game.java
+++ src/app/Game.java
@@ -6,12 +6,12 @@ import java.lang.*;
 
 public class Game {
 
-    private int playerNumber;
-    private List<Player> players;
-    private HashMap<Gem, Integer> bank = new HashMap<Gem, Integer>(Gem.values().length);
-    private Deck[] decks = new Deck[3];
-    private Card[][] market = new Card[3][4];
-    private List<NobleTile> nobles;
+    private static int playerNumber;
+    private static List<Player> players;
+    private static HashMap<Gem, Integer> bank = new HashMap<Gem, Integer>(Gem.values().length);
+    private static Deck[] decks = new Deck[3];
+    private static Card[][] market = new Card[3][4];
+    private static ArrayList<NobleTile> nobles = new ArrayList<>();
 
     public Game(int playerNumber) {
         this.playerNumber = playerNumber;
@@ -26,7 +26,7 @@ public class Game {
 
     }
 
-    public void setPlayerArray(int playerNumber) {
+    public static void setPlayerArray(int playerNumber) {
         Scanner sc = new Scanner(System.in);
         System.out.println("The first player is the youngest.");
         for (int i = 0; i < playerNumber; i++) {
@@ -54,6 +54,49 @@ public class Game {
         }
 
         Game game = new Game(playerNumber);
+    }
+
+    public static void printBoard() {
+        System.out.printf("------------------------------------------------------------------\n");
+        System.out.printf("Bank: ");
+        System.out.printf(bank.get(Gem.Diamond) + "D , ");
+        System.out.printf(bank.get(Gem.Ruby) + "R , ");
+        System.out.printf(bank.get(Gem.Sapphire) + "S , ");
+        System.out.printf(bank.get(Gem.Emerald) + "E , ");
+        System.out.printf(bank.get(Gem.Onyx) + "O , ");
+        System.out.printf(bank.get(Gem.Gold) + "G\n");
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.printf("Deck <%d>\n", i); 
+            for (int j = 1; j <= 4; j++) {
+                System.out.printf("%d.%d %s\n", i, j, market[i][j].toString());
+            }
+        }
+        System.out.printf("------------------------------------------------------------------\n");
+
+        System.out.printf("<NOBLE TILES>\n");  
+        for (int i = 0; i < nobles.size(); i++) {
+            System.out.printf("%s\n", nobles.get(i).toString());
+        }
+        System.out.printf("------------------------------------------------------------------\n\n");
+    }
+
+    // Overloaded printPlayer method 1
+    public static void printPlayer(String name) {
+        // incomplete
+    }
+
+    // Overloaded printPlayer method 2
+    public static void printPlayer(int no) {
 
     }
+
+    public static void printAllPlayers(){
+
+    }
+
+    public static void printCommandList() {
+
+    }
+
 }
